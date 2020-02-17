0ed6b7d2bfe9f2a5bcbebb4e1d0decdd49bbfbf7
mmelihdogan <doganmmelih@gmail.com>
2020-02-17 18:09:44 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+function createEmployeeRecord(employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employee) {
+    let employeeRecords = [];
+    for (let e in employee) {
+        employeeRecords.push(createEmployeeRecord(employee[e]))
+    }
+    return employeeRecords;
+}
