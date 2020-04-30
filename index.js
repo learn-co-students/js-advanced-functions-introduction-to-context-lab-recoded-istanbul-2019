5d0324f26704d13d5413cac02bc800d7ae398862
basil-kawak <basilalisa0@gmail.com>
2020-05-01 02:59:13 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+function createEmployeeRecord([array]) {
+    
+    let obj = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+
+    }
+    return obj 
+}
+
+createEmployeeRecord(["Gray", "Worm", "Security", 1]);
+console.log(createEmployeeRecord(["Gray", "Worm", "Security", 1]))
+
+
+function createEmployeeRecords() {
+   
+
+}
