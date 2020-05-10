808e2ba638ec2e776d58990043b057eb7fa4920b
eliftabak <elifchorghay@gmail.com>
2020-05-10 23:19:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Your code here
+// const employee = ({
+//     firstName: "",
+//     familyName: "",
+//     title: "",
+//     payPerHour: Number,
+// })
+const createEmployeeRecord = function(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents:[],
+        timeOutEvents: [] 
+    }
+}
+const createEmployeeRecords = function(array) {
+    return array.map(arrayOfArray => createEmployeeRecord(arrayOfArray)) 
+}
+
+const createTimeInEvent = (employeeRecord, dateStamp) => {
+    let[date, hour] = dateStamp.split(" ");
+    employeeRecord.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10), 
+        date })
+    return employeeRecord;
+};
+
+const createTimeOutEvent = (employeeRecord, dateStamp) => {
+    let[date, hour] = dateStamp.split(" ");
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return employeeRecord;
+}
+
+const hoursWorkedOnDate = (employeeRecord, dateOftheForm) => {
+    let timeInEvent = employeeRecord.timeInEvents.find(event => event.date === dateOftheForm);
+    let timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === dateOftheForm);
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+}
+
+const wagesEarnedOnDate = (employeeRecord, dateOftheForm) => {
+    return hoursWorkedOnDate(employeeRecord, dateOftheForm) * employeeRecord.payPerHour;
+}
+
+const allWagesFor = (employeeRecord) => {
+    let time = employeeRecord.timeInEvents.map(event => event.date);
+    return time.reduce((total, date) => total + wagesEarnedOnDate(employeeRecord, date), 0)
+}
+
+
+const findEmployeeByFirstName = (srcArray, firstName) => {
+    return (srcArray.find(name => name.firstName === firstName))
+}
+
+const calculatePayroll = (employeeRecord) => {
+    return employeeRecord.reduce((total,element) => total + allWagesFor(element), 0)
+}
