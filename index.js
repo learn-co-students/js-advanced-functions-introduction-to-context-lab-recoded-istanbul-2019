abdfcc68001968cd2fdd1c57b122b084ffde712f
basil-kawak <basilalisa0@gmail.com>
2020-06-08 22:51:25 +0300

    Done.

--- index.js
+++ index.js
@@ -17,37 +17,59 @@ createEmployeeRecord(["Gray", "Worm", "Security", 1]);
 console.log(createEmployeeRecord(["Gray", "Worm", "Security", 1]))
 
 
-function createEmployeeRecords() {
-   
+function createEmployeeRecords(records) {
+   return records.map(record => createEmployeeRecord(record))
 
 }
 
-function createTimeInEvent() {
-
+function createTimeInEvent(obj, str) {
+    const [date, hour] = str.split(' ');
+    obj.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date: date,
+    });
+    return obj;
 }
 
-function createTimeOutEvent() {
-
+function createTimeOutEvent(obj, str) {
+    const [date, hour] = str.split(' ');
+    obj.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date: date,
+    });
+    return obj;
 }
 
-function hoursWorkedOnDate() {
-
+function hoursWorkedOnDate(obj, str) {
+    const timeInEvent = obj.timeInEvents.find(curr => curr.date === str);
+    const timeOutEvent = obj.timeOutEvents.find(curr => curr.date === str);
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+    return hoursWorked;
 }
 
-function wagesEarnedOnDate() {
-
+function wagesEarnedOnDate(obj, str) {
+    const payOwed = hoursWorkedOnDate(obj, str) * obj.payPerHour;
+    return payOwed;
 }
 
-function allWagesFor() {
-
+function allWagesFor(obj) {
+    const allDate = obj.timeInEvents.map(curr => curr.date);
+    const allPayOwed = allDate.reduce((acc, curr) => {
+        return acc + wagesEarnedOnDate(obj, curr);
+    }, 0);
+    return allPayOwed;
 }
 
-function findEmployeeByFirstName() {
-
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(el => el.firstName === firstName);
 }
 
-function calculatePayroll() {
-
+function calculatePayroll(arr) {
+    return arr.reduce((first, now) => {
+        return first + allWagesFor(now);
+    }, 0);
 }
 
  
